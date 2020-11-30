3b3d4471fcc9ddc64312a7e2105a96a97d496265
morganjaouni <morgan.jaouni@ttu.edu>
2020-11-30 12:08:14 -0600

    Adds home account transfer signup routes

--- controllers/signup.js
+++ controllers/signup.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res)=>{
-	res.render('transfer/index.ejs');
+	res.render('account/signup.ejs');
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,29 +1,50 @@
 const express = require('express');
 const app = express();
 const accountController= require('./controllers/account.js')
-
+const PORT = process.env.PORT || 3000;
 
 app.use('/account', accountController);
 
+
+// Controller
+
+
+
+// View Engine
+app.set('view engine', 'ejs');
+
+
+//Home Route
 app.get('/', (req,res) => {
 
     res.render('index.ejs');
 
 })
+
+//Account Show
 app.get('/account', (req,res) => {
 
     res.render('./account/index.ejs');
 
 })
+
+//Transfer Route
 app.get('/transfer', (req,res) => {
 
     res.render('./transfer/index.ejs');
 
 })
 
+//Signup
+app.get('/signup', (req,res) => {
 
-app.listen(3000,() => {
+    res.render('./account/signup.ejs');
 
-    console.log('listening');
+})
+
+
+app.listen(PORT,() => {
+
+    console.log(`Server running on port ${PORT}`);
 
 });
